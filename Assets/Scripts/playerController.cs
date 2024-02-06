283370dc61a6fcfb42c4dc5a140088ddbc5a9652
MichaelR81503 <144148271+MichaelR81503@users.noreply.github.com>
2024-02-06 16:23:01 -0500

    updated

--- Assets/Scripts/playerController.cs
+++ Assets/Scripts/playerController.cs
@@ -44,7 +44,6 @@ public class playerController : MonoBehaviour
         if (collision.gameObject.tag == "Collectible")
         {
             Destroy(collision.gameObject);
-            myScore++;
         }
     }
 }
